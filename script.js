125e4ff5c7bdad3b0174de69fcc6c871eb6eb32b
Lye Yi Xian <lyeyx999@hotmail.com>
2021-12-30 16:58:21 +0800

    Implement playRound function

--- script.js
+++ script.js
@@ -1,10 +1,71 @@
+class Selection {
+    static ROCK = new Selection('Rock');
+    static PAPER = new Selection('Paper');
+    static SCISSORS = new Selection('Scissors');
+
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const computerSelection = [Selection.ROCK, Selection.PAPER, Selection.SCISSORS];
+
 function getRandomInteger(from, to) {
     return Math.floor(Math.random() * (to - from)) + from;
 }
 
 function computerPlay() {
-    arr = ['Rock', 'Paper', 'Scissors'];
-    randomIndex = getRandomInteger(0, arr.length);
+    randomIndex = getRandomInteger(0, computerSelection.length);
 
-    return arr[randomIndex];
+    return computerSelection[randomIndex].name;
+}
+
+function capitalize(str) {
+    lowercase = str.slice(1).toLowerCase();
+    uppercase = str.charAt(0).toUpperCase();
+
+    return uppercase + lowercase;
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+    playerInput = capitalize(playerSelection);
+    const win = `You Win! ${playerInput} beats ${computerSelection}`;
+    const lose = `You Lose! ${computerSelection} beats ${playerInput}`;
+    const tie = "It's a tie!";
+
+    switch (playerInput) {
+        case Selection.ROCK.name:
+            if (computerSelection === Selection.PAPER.name) {
+                return lose;
+            }
+
+            if (computerSelection === Selection.SCISSORS.name) {
+                return win;
+            }
+            break;
+        case Selection.PAPER.name:
+            if (computerSelection === Selection.ROCK.name) {
+                return win;
+            }
+
+            if (computerSelection === Selection.SCISSORS.name) {
+                return lose;
+            }
+            break;
+        case Selection.SCISSORS.name:
+            if (computerSelection === Selection.ROCK.name) {
+                return lose;
+            }
+
+            if (computerSelection === Selection.PAPER.name) {
+                return win;
+            }
+            break;    
+        default:
+            return "Please input either Rock, Paper or Scissors.";
+    }
+
+    return tie;
 }
